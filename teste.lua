0d2891f5e14855317e5ce2d82ac587463c469836
𝐺𝑢𝑖𝑙ℎ𝑒𝑟𝑚𝑒 <150200952+GuilhermeXVII@users.noreply.github.com>
2025-10-05 18:34:05 -0300

    Add files via upload

---
+++ teste.lua
@@ -0,0 +1,37 @@
+print("Olá mundo")
+
+
+guilherme = true
+type(guilherme)
+print(type(guilherme))
+
+
+idade = 18
+type(idade)
+print(type(idade))
+
+melhorCarro = "Ômega";
+string.reverse(melhorCarro)
+print(string.reverse(melhorCarro))  
+
+function exibeMensagem()
+    print("Olá pessoal")
+end
+
+exibeMensagem()
+
+imprimeMensagem = function()
+    print("Olá guilherme")
+end
+
+imprimeMensagem()
+print(type(exibeMensagem))
+
+carro = {"Ômega", "Camaro", "Ferrari"}
+print(#carro)
+
+print(carro[1])
+print(#carro[1])
+
+print(carro[2])
+print(#carro[2])
